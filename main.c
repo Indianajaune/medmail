9a64118c527e35ecc8f051b606b535fae5d3616a
Abdelkhalek El Omari <aetechnology@live.com>
2019-12-06 20:48:26 +0100

    Dev. partie suppression et autres ameliorations

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #define MAX_CHAR_SIZE 500
 
 typedef struct{
+  int id;
   char prenom[MAX_CHAR_SIZE];
   char nom[MAX_CHAR_SIZE];
   char adresse[MAX_CHAR_SIZE];
@@ -30,11 +31,10 @@ void loginAdmin(char*);
 void saisirCoordonnes();
 void listerCoordonnees();
 void rechercherCoordonnees();
-void rechercherParNom();
-void rechercherParPrenom();
-void rechercherParAdresse();
-void rechercherParPriorite();
+void lancerRechercherCoordonnees(int);
+void supprimerCoordonnee();
 
+int getLastIdCoord();
 
 int main(){
   char mdpAdmin[MAX_CHAR_SIZE] = "n";
@@ -142,7 +142,7 @@ void afficherMenuAdmin(char *mdp)
                 break;
            case 3: rechercherCoordonnees(); afficherMenuAdmin(tmp_ptr_mdp);
                break;
-           case 4: printf("4");
+           case 4: supprimerCoordonnee(); afficherMenuAdmin(tmp_ptr_mdp);
                break;
            case 5: printf("5");
                 break;
@@ -160,8 +160,30 @@ void afficherMenuAdmin(char *mdp)
     } while (choix != 9);
 }
 
+int getLastIdCoord()
+{
+  int lastId = 0;
+  FILE *sourceBdd;
+  sourceBdd = fopen("coordonnees.dat", "r");
+  if (sourceBdd == NULL)
+  {
+    fprintf(stderr, "\nImpossible ouvrir la base de données!\n");
+    exit(0);
+  }
+
+  coordonnees coord_temp;
+
+  while(fread(&coord_temp, sizeof(coordonnees), 1, sourceBdd))
+  {
+    lastId = coord_temp.id;
+  }
+
+  return lastId + 1;
+}
+
 void saisirCoordonnes()
 {
+  int last_id = getLastIdCoord();
   FILE *sourceBdd;
   sourceBdd = fopen("coordonnees.dat", "a");
   if (sourceBdd == NULL)
@@ -180,16 +202,18 @@ void saisirCoordonnes()
 
     coordonnees coord_temp;
 
-    printf("Veuillez saisir le prénom : ");
+    coord_temp.id = last_id;
+
+    printf("\nVeuillez saisir le prénom : ");
     scanf("%s", coord_temp.prenom);
 
-    printf("Veuillez saisir le nom : ");
+    printf("\nVeuillez saisir le nom : ");
     scanf("%s", coord_temp.nom);
 
-    printf("Veuillez saisir l'adresse : ");
+    printf("\nVeuillez saisir l'adresse : ");
     scanf("%s", coord_temp.adresse);
 
-    printf("Veuillez saisir l'importance du client (0 ou 1) : ");
+    printf("\nVeuillez saisir l'importance du client (0 ou 1) : ");
     scanf("%d", &(coord_temp.importance));
 
     fwrite (&coord_temp, sizeof(coordonnees), 1, sourceBdd);
@@ -200,6 +224,11 @@ void saisirCoordonnes()
       scanf(" %c", &choix);
     } while (choix != 'o' && choix != 'n');
 
+    if (choix == 'o')
+    {
+      last_id++;
+    }
+
   } while (choix != 'n');
   fclose(sourceBdd);
 }
@@ -208,7 +237,7 @@ void listerCoordonnees()
 {
   system("clear");
   int compteur = 0;
-  char key;
+  char choix;
   FILE *sourceBdd;
   sourceBdd = fopen("coordonnees.dat", "r");
   if (sourceBdd == NULL)
@@ -225,7 +254,7 @@ void listerCoordonnees()
 
   while(fread(&coord_temp, sizeof(coordonnees), 1, sourceBdd))
   {
-    printf("\nPrénom : %s \nNom : %s \nAdresse mail: %s \nPriorite : %d\n", coord_temp.prenom, coord_temp.nom, coord_temp.adresse, coord_temp.importance);
+    printf("\nId : %d \nPrénom : %s \nNom : %s \nAdresse mail: %s \nPriorite : %d\n", coord_temp.id, coord_temp.prenom, coord_temp.nom, coord_temp.adresse, coord_temp.importance);
     printf("\n---------------------\n");
     compteur++;
   }
@@ -235,8 +264,11 @@ void listerCoordonnees()
 
   fclose(sourceBdd);
 
-  printf("Cliquer sur une touche pour revenir au menu. ");
-  scanf(" %c", &key);
+  printf("Cliquer sur la touche 'e' pour revenir au menu principal. ");
+  do
+  {
+    scanf(" %c", &choix);
+  } while (choix != 'e');
 }
 
 void rechercherCoordonnees()
@@ -254,21 +286,21 @@ void rechercherCoordonnees()
 
      printf("1) Rechercher par nom.\n");
      printf("2) Recherche par prenom.\n");
-     printf("3) Rechercher par adresse mail.\n\n");
-     printf("4) Rechercher par priorité.\n\n");
+     printf("3) Rechercher par adresse mail.\n");
+     printf("4) Rechercher par priorité.\n");
      printf("5) Returner au menu principal.\n\n");
      printf("Veuillez choisir u parametre de recherche parmis ceux proposés ci-dessus : ");
      scanf(" %d",&choix);
 
      switch (choix)
      {
-         case 1: rechercherParNom();
+         case 1: lancerRechercherCoordonnees(1); system("clear");
               break;
-         case 2: rechercherParPrenom();
+         case 2: lancerRechercherCoordonnees(2); system("clear");
               break;
-         case 3: rechercherParAdresse();
+         case 3: lancerRechercherCoordonnees(3); system("clear");
              break;
-         case 4: rechercherParPriorite();
+         case 4: lancerRechercherCoordonnees(4); system("clear");
              break;
          case 5: exit = true;
              break;
@@ -278,7 +310,179 @@ void rechercherCoordonnees()
   } while (choix != 5);
 }
 
-void rechercherParNom(){}
-void rechercherParPrenom(){}
-void rechercherParAdresse(){}
-void rechercherParPriorite(){}
+void lancerRechercherCoordonnees(int type)
+{
+  system("clear");
+  char nom[MAX_CHAR_SIZE];
+  char choix;
+  int compteur = 0;
+  FILE *sourceBdd;
+  sourceBdd = fopen("coordonnees.dat", "r");
+  if (sourceBdd == NULL)
+  {
+    fprintf(stderr, "\nImpossible ouvrir la base de données!\n");
+    exit(0);
+  }
+
+  coordonnees coord_temp;
+
+  if (type == 1)
+  {
+    printf("Veuillez rentrer le nom : ");
+    scanf("%s", nom);
+  }
+  else if (type == 2)
+  {
+    printf("Veuillez rentrer le prenom : ");
+    scanf("%s", nom);
+  }
+  else if (type == 3)
+  {
+    printf("Veuillez rentrer l'adresse : ");
+    scanf("%s", nom);
+  }
+  else
+  {
+    do
+    {
+      printf("Veuillez rentrer le niveau d'importance (1 ou 0) : ");
+      scanf("%s", nom);
+    } while (strcmp(nom, "1") != 0 && strcmp(nom, "0") != 0);
+  }
+
+  while(fread(&coord_temp, sizeof(coordonnees), 1, sourceBdd))
+  {
+    if (type == 1)
+    {
+      if (strcmp(nom, coord_temp.nom) == 0)
+      {
+        printf("\nPrénom : %s \nNom : %s \nAdresse mail: %s \nPriorite : %d\n", coord_temp.prenom, coord_temp.nom, coord_temp.adresse, coord_temp.importance);
+        printf("\n---------------------\n");
+        compteur++;
+      }
+    }
+    else if (type == 2)
+    {
+      if (strcmp(nom, coord_temp.prenom) == 0)
+      {
+        printf("\nPrénom : %s \nNom : %s \nAdresse mail: %s \nPriorite : %d\n", coord_temp.prenom, coord_temp.nom, coord_temp.adresse, coord_temp.importance);
+        printf("\n---------------------\n");
+        compteur++;
+      }
+    }
+    else if (type == 3)
+    {
+      if (strcmp(nom, coord_temp.adresse) == 0)
+      {
+        printf("\nPrénom : %s \nNom : %s \nAdresse mail: %s \nPriorite : %d\n", coord_temp.prenom, coord_temp.nom, coord_temp.adresse, coord_temp.importance);
+        printf("\n---------------------\n");
+        compteur++;
+      }
+    }
+    else
+    {
+      if (atoi(nom) == coord_temp.importance)
+      {
+        printf("\nPrénom : %s \nNom : %s \nAdresse mail: %s \nPriorite : %d\n", coord_temp.prenom, coord_temp.nom, coord_temp.adresse, coord_temp.importance);
+        printf("\n---------------------\n");
+        compteur++;
+      }
+    }
+
+  }
+
+  if(compteur > 0)
+  {
+    printf("Coordonnées trouvées en total : %d.", compteur);
+    printf("\n---------------------\n\n");
+  }
+  else
+  {
+    printf("\n---------------------\n");
+    printf("Aucune coordonnée trouvée avec ce critere de recherche.");
+    printf("\n---------------------\n\n");
+  }
+
+  fclose(sourceBdd);
+
+  printf("Cliquer sur la touche 'e' pour revenir au menu de recherche. ");
+  do
+  {
+    scanf(" %c", &choix);
+  } while (choix != 'e');
+}
+
+void supprimerCoordonnee()
+{
+  system("clear");
+  int id;
+  bool found = false;
+  char choix;
+
+  coordonnees coord_temp;
+
+  FILE *tmp_sourceBdd, *sourceBdd;
+  tmp_sourceBdd = fopen("temp_coordonnees.dat", "a");
+  sourceBdd = fopen("coordonnees.dat", "r");
+  if (tmp_sourceBdd == NULL && sourceBdd == NULL)
+  {
+    fprintf(stderr, "\nImpossible ouvrir la base de données temporaire (suppression coordonnee)!\n");
+    exit(0);
+  }
+  else
+  {
+    do
+    {
+      system("clear");
+      if (choix == 'n')
+      {
+        break;
+      }
+
+      printf("\nVeuillez rentrer l'id de la coordonnée à supprimer : ");
+      scanf(" %d", &id);
+
+      while(fread(&coord_temp, sizeof(coordonnees), 1, sourceBdd))
+      {
+        if(id == coord_temp.id)
+        {
+          found = true;
+        }
+      }
+
+      fclose(sourceBdd);
+      sourceBdd = fopen("coordonnees.dat", "r");
+
+      if(found == true)
+      {
+        while(fread(&coord_temp, sizeof(coordonnees), 1, sourceBdd))
+        {
+          if(id != coord_temp.id)
+          {
+            fwrite(&coord_temp, sizeof(coordonnees), 1, tmp_sourceBdd);
+          }
+        }
+        fclose(sourceBdd);
+        remove("coordonnees.dat");
+        fclose(tmp_sourceBdd);
+        rename("temp_coordonnees.dat", "coordonnees.dat");
+
+        do
+        {
+          printf("\nSupprimée avec succès! Voulez-vous supprimer une autre coordonnee? (o/n) ");
+          scanf(" %c", &choix);
+        } while (choix != 'o' && choix != 'n');
+      }
+      else
+      {
+        printf("\nL'id que vous avez rentré n'hexiste pas dans la base de donées des coordonnées!");
+
+        do
+        {
+          printf("\nVoulez-vous réessayer avec un autre id? (o/n) ");
+          scanf(" %c", &choix);
+        } while (choix != 'o' && choix != 'n');
+      }
+    } while(choix != 'n');
+  }
+}
